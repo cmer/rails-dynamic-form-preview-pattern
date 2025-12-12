d54e1a863d57d4ab4c0805b112101e61824ce4a6
Carl Mercier <carl@carlmercier.com>
2025-12-12 17:06:48 -0600

    Added preview

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def render_markdown(markdown_text)
+    return "".html_safe if markdown_text.blank?
+
+    require "rdoc"
+
+    document = RDoc::Markdown.new.parse(markdown_text.to_s)
+    RDoc::Markup::ToHtml.new(RDoc::Options.new).convert(document).html_safe
+  end
 end
